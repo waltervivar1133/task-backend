d8fb5515f17f2d495c77748224fe64d5217dcf25
Walter Vivar <waltervivar1133@gmail.com>
2021-06-09 23:06:50 -0500

    FIX: ajustes para deploy

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ app.use('/api/tareas', require('./routes/tareas'))
 
 
 app.listen(port, '0.0.0.0' , () => {
-  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
 })
