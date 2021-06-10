ff9eb699bce3723dbee25bf7540219e430e36228
Walter Vivar <waltervivar1133@gmail.com>
2021-06-09 23:22:55 -0500

    FIX: ajustes para deploy

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 app.use(express.json({ extended : true }))
 
-const port = process.env.port || 4000; 
+const PORT = process.env.PORT || 4000; 
 
 //rutas 
 
@@ -34,6 +34,6 @@ app.use('/api/tareas', require('./routes/tareas'))
 
 
 
-app.listen(port, '0.0.0.0' , () => {
-  console.log(`El servidor esta funcionando en el puerto ${port}`);
+app.listen(PORT, '0.0.0.0' , () => {
+  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 })
